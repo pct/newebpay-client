78c481b4604f410267f769ac2eda960b10eae084
Jin-Sih Lin <linpct@gmail.com>
2023-03-02 21:14:56 +0800

    feat: 增加查詢單筆訂單的 API

---
+++ lib/newebpay/query_trade_info.rb
@@ -0,0 +1,76 @@
+# frozen_string_literal: true
+
+require_relative 'config'
+require_relative 'errors'
+require_relative './AES/cryptographic'
+require_relative './SHA256/cryptographic'
+
+module Newebpay
+  class QueryTradeInfo
+    attr_accessor :trade_info
+    attr_reader :response
+
+    def initialize(
+      order_number: nil,
+      amount: nil,
+      gateway: nil
+    )
+      unless order_number && amount
+        raise Newebpay::PaymentArgumentError,
+          '請確認以下參數皆有填寫:
+          - order_number
+          - amount
+          '
+      end
+
+      @key = Config.options[:HashKey]
+      @iv = Config.options[:HashIV]
+
+      @order_number = order_number
+      @amount = amount
+      @gateway = gateway
+
+      set_trade_info
+      set_check_value
+    end
+
+    def request!
+      uri = URI("#{self.api_base_url}/API/QueryTradeInfo")
+
+      res = Net::HTTP.post_form(uri,
+        MerchantID: Config.options[:MerchantID], # 若要用複合式商店代號(MS5 開頭)，則[Gateway]參數為必填
+        Version: '1.3',
+        RespondType: 'JSON',
+        CheckValue: @check_value,
+        TimeStamp: Time.now.to_i.to_s,
+        MerchantOrderNo: @order_number,
+        Amt: @amount.to_i,
+        Gateway: @gateway #若為複合式商店(MS5 開頭) ，此欄位為必填
+      )
+
+      @response = JSON.parse(res.body)
+    end
+
+    def success?
+      return if @response.nil?
+
+      @response['Status'] == 'SUCCESS'
+    end
+
+    private
+
+    def set_trade_info
+      # 變數順序不能變，要作為 check_value
+      @trade_info = {
+        Amt: @amount.to_i,
+        MerchantID: Config.options[:MerchantID],
+        MerchantOrderNo: @order_number
+      }
+    end
+
+    def set_check_value
+      url_encoded_trade_info = URI.encode_www_form(@trade_info)
+      @check_value = @sha256_trade_info = SHA256::Cryptographic.new(url_encoded_trade_info).encrypt
+    end
+  end
+end
