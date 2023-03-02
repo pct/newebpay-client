74dc7089a748a27581a0626ba6a29307e1f8b4be
Jin-Sih Lin <linpct@gmail.com>
2023-03-02 20:02:49 +0800

    feat: payment 加入回傳網址

--- lib/newebpay/payment.rb
+++ lib/newebpay/payment.rb
@@ -8,15 +8,41 @@ require_relative './SHA256/cryptographic'
 module Newebpay
   class Payment
     attr_accessor :trade_info
+    attr_reader :response
 
-    def initialize(order_number: nil, amount: nil, product_description: '產品說明', email: '', order_comment: '')
-      unless order_number && amount
+    def initialize(
+      # 必填客製網址(同一個站有不同接收的 route 時使用)
+      return_url: nil,
+      notify_url: nil,
+      back_url: nil,
+
+      # 必填參數
+      order_number: nil,
+      amount: nil,
+
+      product_description: '產品說明',
+      email: '',
+      order_comment: ''
+    )
+      unless return_url && notify_url && back_url && order_number && amount
         raise Newebpay::PaymentArgumentError,
-              'Please make sure that your arguments (order_number, amount) are filled in correctly'
+          '請確認以下參數皆有填寫:
+          - return_url
+          - notify_url
+          - back_url
+
+          - order_number
+          - amount
+        '
       end
+
 
       @key = Config.options[:HashKey]
       @iv = Config.options[:HashIV]
+
+      @return_url = return_url
+      @notify_url = notify_url
+      @back_url = back_url
 
       @order_number = order_number
       @order_comment = order_comment
@@ -39,6 +65,18 @@ module Newebpay
       }
     end
 
+    def request!
+      uri = URI("#{self.api_base_url}/MPG/mpg_gateway")
+      res = Net::HTTP.post_form(uri, MerchantID_: Config.options[:MerchantID], PostData_: @post_data)
+      @response = JSON.parse(res.body)
+    end
+
+    def success?
+      return if @response.nil?
+
+      @response['Status'] == 'SUCCESS'
+    end
+
     private
 
     def url_encoded_trade_info
@@ -50,6 +88,10 @@ module Newebpay
       @trade_info = options.transform_keys(&:to_sym)
 
       individual_trade_info = {
+        ReturnURL: @return_url,
+        NotifyURL: @notify_url,
+        ClientBackURL: @back_url,
+
         MerchantOrderNo: @order_number, # 商店訂單編號，如：用途_日期時間戳記_流水號
         Amt: @amount.to_i, # 訂單金額
         ItemDesc: @product_description, # 商品資訊 (50 字內)
