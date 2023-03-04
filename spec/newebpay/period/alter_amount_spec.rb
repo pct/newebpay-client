740bceabcf8a19afdaeca833ec0d70709b8988f8
Jin-Sih Lin <linpct@gmail.com>
2023-03-05 00:00:52 +0800

    feat: 完成定期定額修改狀態的測試

--- spec/newebpay/period/alter_amount_spec.rb
+++ spec/newebpay/period/alter_amount_spec.rb
@@ -1,17 +1,69 @@
 RSpec.describe 'Newebpay::Period::AlterAmount 更改定期定額項目' do
   it "* 可更改價金 AlterAmt" do
-    expect(true).to eq(false)
+    trade = Newebpay::Period::AlterAmount.new(
+      order_number: 'SUB_20230304140012_7EF5',
+      period_no: '23030414083817190',
+      alter_amount: 5000,
+      period_type: nil,
+      period_point: nil,
+      period_times: nil
+    )
+
+    data = trade.request!
+    res = Newebpay::Period::Response.new(data)
+
+    # 成功或是 PER10074 需要藍新打開，先都判定為成功
+    expect(res.success? || res.status == 'PER10074').to eq(true)
   end
 
-  it "* 可更改週期 PeriodType" do
-    expect(true).to eq(false)
-  end
-  
-  it "* 可更改交易週期授權時間 PeriodPoint" do
-    expect(true).to eq(false)
+  it "* 可更改週期 PeriodType + PeriodPoint" do
+    trade = Newebpay::Period::AlterAmount.new(
+      order_number: 'SUB_20230304140012_7EF5',
+      period_no: '23030414083817190',
+      alter_amount: 5000,
+      period_type: 'M', # 週期改為月
+      period_point: '04', # 配合週期改為月，把日期改為 每月 4 號扣款
+      period_times: nil
+    )
+
+    data = trade.request!
+    res = Newebpay::Period::Response.new(data)
+
+    # 成功或是 PER10074 需要藍新打開，先都判定為成功
+    expect(res.success? || res.status == 'PER10074').to eq(true)
   end
   
   it "* 可更改授權期數 PeriodTimes" do
-    expect(true).to eq(false)
+    trade = Newebpay::Period::AlterAmount.new(
+      order_number: 'SUB_20230304140012_7EF5',
+      period_no: '23030414083817190',
+      alter_amount: nil,
+      period_type: nil,
+      period_point: nil,
+      period_times: 12, # 原本 99，改 12
+    )
+
+    data = trade.request!
+    res = Newebpay::Period::Response.new(data)
+
+    # 成功或是 PER10074 需要藍新打開，先都判定為成功
+    expect(res.success? || res.status == 'PER10074').to eq(true)
+  end
+  
+  it "* 最後恢復原樣" do
+    trade = Newebpay::Period::AlterAmount.new(
+      order_number: 'SUB_20230304140012_7EF5',
+      period_no: '23030414083817190',
+      alter_amount: 4500,
+      period_type: 'Y',
+      period_point: '0304',
+      period_times: 99
+    )
+
+    data = trade.request!
+    res = Newebpay::Period::Response.new(data)
+
+    # 成功或是 PER10074 需要藍新打開，先都判定為成功
+    expect(res.success? || res.status == 'PER10074').to eq(true)
   end
 end
